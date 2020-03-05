00b457bce876710507964a215874d78027761fb0
Julia Cox <juliacox@dyn-128-128-226-184.whoi.edu>
2020-03-05 16:32:58 -0500

    Changed the way I edited the dates

--- lter_zooplankton_1.R
+++ lter_zooplankton_1.R
@@ -5,11 +5,9 @@ EcoMon_Plankton_Data_v3_1_2_ <- read_excel("~/Desktop/NES-LTER/Zoop/EcoMon_Plank
 
 EcoMon <- EcoMon_Plankton_Data_v3_1_2_
 
-EcoMon_ymd <- EcoMon %>% separate(date, sep ="-", into = c("year", "month", "day"))
-#separate date into year, month, and day 
 install.packages("data.table")
 library(data.table)
-nes_lat <- setDT(EcoMon_ymd)[!(lat %between% c(0, 39.5) | lat %between% c(41.4, 50))]
+nes_lat <- setDT(EcoMon)[!(lat %between% c(0, 39.5) | lat %between% c(41.4, 50))]
 #only include latitudes between 39.5 and 41.4 
 range(nes_lat$lat)
 #view range of latitudes to make sure it worked 
@@ -19,7 +17,7 @@ range(nes$lat)
 range(nes$lon)
 #view nes (northeast us shelf) lat and lon to make sure it worked 
 
-nes_zoo_wide <- nes[,c("cruise_name","year","month","day","station","zoo_gear","ich_gear","lat","lon","time","depth","sfc_temp","sfc_salt","btm_temp","btm_salt","volume_1m2","calfin_10m2","ctyp_10m2","pseudo_10m2","tlong_10m2","larvaceans_10m2","para_10m2")]
+nes_zoo_wide <- nes[,c("cruise_name","date","station","zoo_gear","ich_gear","lat","lon","time","depth","sfc_temp","sfc_salt","btm_temp","btm_salt","volume_1m2","calfin_10m2","ctyp_10m2","pseudo_10m2","tlong_10m2","larvaceans_10m2","para_10m2")]
 #make a vector with desired columns 
 #select only the six taxa of interest (calanus finmarchicus, centropages typicus, 
 #pseudocalanus, paracalanus, temora longicornis, and larvacians)
@@ -32,8 +30,13 @@ library("Amelia")
 missmap(nes_zoo_long)
 #see what's missing in data with missingness map 
 plot(nes_zoo_wide$date, type = 'o', pch = '|', ylab = '')
-#attempt to see what dates are missing
+#attempt to see what dates are missing, didn't work 
 
-View(nes_zoo_long)
+nes_zoo_wide_ymd <- nes_zoo_wide %>% separate(date, sep ="-", into = c("year", "month", "day"))
+#separate date into year, month, and day 
 
 
+library(ggplot2)
+ggplot(nes_zoo_wide, aes(x=date, y=calfin_10m2)) + geom_smooth() #Calanus finmarchicus 
+
+View(nes_zoo_wide)
