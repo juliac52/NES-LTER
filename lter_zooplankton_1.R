df876972ed620a9df8ca538324343247add35cd2
Julia Cox <juliacox@dyn-128-128-226-184.whoi.edu>
2020-03-05 15:55:47 -0500

    Added separate columns for year, month, and day Experimented with seeing gaps in data

--- lter_zooplankton_1.R
+++ lter_zooplankton_1.R
@@ -5,9 +5,11 @@ EcoMon_Plankton_Data_v3_1_2_ <- read_excel("~/Desktop/NES-LTER/Zoop/EcoMon_Plank
 
 EcoMon <- EcoMon_Plankton_Data_v3_1_2_
 
+EcoMon_ymd <- EcoMon %>% separate(date, sep ="-", into = c("year", "month", "day"))
+#separate date into year, month, and day 
 install.packages("data.table")
 library(data.table)
-nes_lat <- setDT(EcoMon)[!(lat %between% c(0, 39.5) | lat %between% c(41.4, 50))]
+nes_lat <- setDT(EcoMon_ymd)[!(lat %between% c(0, 39.5) | lat %between% c(41.4, 50))]
 #only include latitudes between 39.5 and 41.4 
 range(nes_lat$lat)
 #view range of latitudes to make sure it worked 
@@ -17,10 +19,21 @@ range(nes$lat)
 range(nes$lon)
 #view nes (northeast us shelf) lat and lon to make sure it worked 
 
-nes_zoo_wide <- nes[,c("cruise_name","station","zoo_gear","ich_gear","lat","lon","date","time","depth","sfc_temp","sfc_salt","btm_temp","btm_salt","volume_1m2","calfin_10m2","ctyp_10m2","pseudo_10m2","tlong_10m2","larvaceans_10m2","para_10m2")]
+nes_zoo_wide <- nes[,c("cruise_name","year","month","day","station","zoo_gear","ich_gear","lat","lon","time","depth","sfc_temp","sfc_salt","btm_temp","btm_salt","volume_1m2","calfin_10m2","ctyp_10m2","pseudo_10m2","tlong_10m2","larvaceans_10m2","para_10m2")]
 #make a vector with desired columns 
 #select only the six taxa of interest (calanus finmarchicus, centropages typicus, 
 #pseudocalanus, paracalanus, temora longicornis, and larvacians)
+
 library(tidyr)
 nes_zoo_long <- gather(nes_zoo_wide, taxa, organisms_per_10m2, calfin_10m2:para_10m2, factor_key=TRUE)
 #convert to long format 
+install.packages("Amelia")
+library("Amelia")
+missmap(nes_zoo_long)
+#see what's missing in data with missingness map 
+plot(nes_zoo_wide$date, type = 'o', pch = '|', ylab = '')
+#attempt to see what dates are missing
+
+View(nes_zoo_long)
+
+
